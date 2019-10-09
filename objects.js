786eb4d194b9f9ec298d70da839f9dc4c3c27604
Zahran Kabir <zahrank@gmail.com>
2019-10-08 17:55:18 -0700

    solved;

---
+++ objects.js
@@ -0,0 +1,9 @@
+let playlist = {zahran: "nice song"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName]
+}
